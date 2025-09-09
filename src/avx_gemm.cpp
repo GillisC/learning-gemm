bd7e8ffa8d40a7fc663116ef8e2b539389b7657f
Tim Carlsson <timcarlsson0203@gmail.com>
2025-09-09 21:34:11 +0200

    readme additions

--- src/avx_gemm.cpp
+++ src/avx_gemm.cpp
@@ -4,6 +4,26 @@
 #include "immintrin.h"
 #include "matrix.h"
 
+void transpose_data(const double *a_data, int lda, double *b_data, int ldb,
+                    int M, int N)
+{
+    int block_size = 8;
+
+    for (int ii = 0; ii < M; ii += block_size)
+    {
+        for (int jj = 0; jj < N; jj += block_size)
+        {
+            for (int i = ii; i < std::min(ii + block_size, M); ++i)
+            {
+                for (int j = jj; j < std::min(jj + block_size, N); ++j)
+                {
+                    b_data[j * ldb + i] = a_data[i * lda + j];
+                }
+            }
+        }
+    }
+}
+
 void gemm(const int M, const int N, const int K, const double *A, const int lda,
           const double *B, const int ldb, double *C, const int ldc,
           const double alpha, const double beta)
@@ -13,9 +33,9 @@ void gemm(const int M, const int N, const int K, const double *A, const int lda,
 
     Matrix T_B;
     init_matrix(&T_B, N, K);
-    transpose(const Matrix *a, Matrix *b)
+    transpose_data(B, ldb, T_B.data, T_B.stride, M, K);
 
-        for (int ii = 0; ii < M; ii += block_size)
+    for (int ii = 0; ii < M; ii += block_size)
     {
         for (int jj = 0; jj < N; jj += block_size)
         {
